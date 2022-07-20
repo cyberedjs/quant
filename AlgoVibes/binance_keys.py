e4fb998f8ad84caa742fc241fa73b06c8a2993dd
cyberdjs <joonsuheojs@gmail.com>
2022-07-20 08:25:04 +0000

    commiting all the works

---
+++ AlgoVibes/binance_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+api_key = "ncaGKCQNu9KS8kEwF3Jri37YjsOkKCfLGiKZcq87iThEU3QZIWdxOyeQLUvOCPpG"
+secret_key = "tvSQ36Idx7W1QwiMx5ycduhziLQZaHUbhnHZGhqlSRaF6VrkP8QqsLY3LCidthdJ"
+
